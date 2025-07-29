7a0cf995afd71a0ccb41cff73c7698e9323c794c
Ahmed5Atiya <atiya2892@gmail.com>
2025-07-29 03:21:31 +0300

    frist i create the sehema for the user 2- create the route for singup 3- create tje controller to create the function for the user 4- user the nodemiler for send the code to gmail the user 5- generate the token and send it for the frontend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,77 @@
 const express = require("express");
-const { Signup } = require("../controller/user.controller");
-
 const router = express.Router();
 
-router.post("/signup", Signup);
+const userController = require("../controller/user.controller");
+const authValidation = require("../validation/authValidation");
+
+// Auth routes config for better reusability and clarity
+const routes = [
+  {
+    method: "post",
+    path: "/signup",
+    middlewares: [authValidation.SignUpValidation],
+    handler: userController.Signup,
+  },
+  {
+    method: "put",
+    path: "/verify-email",
+    middlewares: [
+      userController.ProtectedRoute,
+      authValidation.VerifyEmailValidation,
+    ],
+    handler: userController.VerifyEmail,
+  },
+  {
+    method: "post",
+    path: "/resend-verification",
+    middlewares: [
+      userController.ProtectedRoute,
+      authValidation.ResendCodeValidation,
+    ],
+    handler: userController.ResendVerificationCode,
+  },
+  {
+    method: "post",
+    path: "/login",
+    middlewares: [authValidation.LoginValidation],
+    handler: userController.Login,
+  },
+  {
+    method: "post",
+    path: "/forgot-password",
+    middlewares: [authValidation.ForgetPasswordValidation],
+    handler: userController.ForgetPassword,
+  },
+  {
+    method: "post",
+    path: "/reset-password-code",
+    middlewares: [
+      userController.ProtectedRoute,
+      authValidation.ResetPasswordCodeValidation,
+    ],
+    handler: userController.ResetPasswordCode,
+  },
+  {
+    method: "put",
+    path: "/reset-password",
+    middlewares: [
+      userController.ProtectedRoute,
+      authValidation.ResetPasswordValidation,
+    ],
+    handler: userController.resetPassword,
+  },
+  // Example for future: get all users
+  // {
+  //   method: "get",
+  //   path: "/",
+  //   middlewares: [userController.ProtectedRoute],
+  //   handler: userController.getAllUsers,
+  // },
+];
+
+// Register all routes dynamically
+routes.forEach(({ method, path, middlewares, handler }) => {
+  router[method](path, ...middlewares, handler);
+});
 
 module.exports = router;
