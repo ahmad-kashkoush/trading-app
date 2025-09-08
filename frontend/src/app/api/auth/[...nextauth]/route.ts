a511f073d345f3a6b11aed9b501628d65e60db52
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-09-08 23:43:05 +0300

    feat: expose types using module augmentation

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,19 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { GithubProfile } from "next-auth/providers/github";
 const options: NextAuthOptions = {
     providers: [
         GithubProvider({
+            profile(profile: GithubProfile) {
+                return {
+                    ...profile, 
+                    id: profile.id.toString(),
+                    image: profile.avatar_url,
+                    name: profile.name,
+                    role: "user",
+                }
+            },
             // assertion (as type) is telling TS that you know better.
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
@@ -19,7 +29,8 @@ const options: NextAuthOptions = {
                 const userToPass = {
                     id: "1",
                     name: "user-to-pass", 
-                    email: "user-to-pass@gmail.com"
+                    email: "user-to-pass@gmail.com",
+                    role: "user",
                 };
                 if(credentials?.username === "user-to-pass" && credentials?.password === "123") {
                     return userToPass;
@@ -29,6 +40,20 @@ const options: NextAuthOptions = {
     }),
        
     ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if(user) {
+                token.role = user.role;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if(session.user) {
+                session.user.role = token.role;
+            }
+            return session;
+        }
+    }
 }
 
 const handler = NextAuth(options);
