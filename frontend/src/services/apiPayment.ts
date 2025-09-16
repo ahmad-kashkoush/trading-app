d589e454cce8cba8bec9e0b95c4259faa74b51bc
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-09-16 17:30:14 +0300

    feat(frontend): add stripe setup skeleton

---
+++ frontend/src/services/apiPayment.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const apiPayment = {
+
+}
+
+
+
+
+
+
+
+
+
+
+export default apiPayment;
